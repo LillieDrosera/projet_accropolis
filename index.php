a80b420ad1a2a53bea07d6c911b46560a11d3074
LillieDrosera <lilliedrosera@gmail.com>
2017-02-24 08:58:59 +0100

    update footer

--- index.php
+++ index.php
@@ -6,15 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
 
-    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="//localhost:35729/livereload.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- <script type="text/javascript" src="//localhost:35729/livereload.js"></script> -->
+    <script src="./assets/js/script.js" charset="utf-8"></script>
+
 
     <title>Accropolis | La chaîne de streaming citoyenne</title>
+
 </head>
 
-
+<?php include('template/header.html') ?>
 
 <body class="container-fluid">
     <main class="col-md-9">
@@ -24,7 +30,7 @@
                     <h3>Ne ratez aucun Live !</h3>
                     <p>Téléchargez votre extension sur</p>
                     <p>
-                        <a class="AC-extend" href="#">
+                        <a class="AC-extend" href="https://chrome.google.com/webstore/detail/accropolis/mhncadmiacfmkolcclpdbfnbjgadnhmg?hl=fr">
                             <svg class="AC-extend" height="32" width="32" xmlns="http://www.w3.org/2000/svg">
                                 <path class="AC-extend" d="M15.987 9.991c3.314 0 6 2.688 6 6 0 3.315-2.686 6-6 6-3.312 0-6-2.686-6-6 0-3.312 2.688-6 6-6z" fill="#888"/>
                                 <path class="AC-extend" d="M15.987 23.992c-4.048 0-7.385-3.008-7.917-6.912l-5.515-9.563-.006-.014-.617.891c-.152.279-.3.562-.436.851l-.295.69c-.098.238-.195.479-.282.725-.098.273-.183.549-.264.826l-.178.637c-.074.297-.137.594-.194.893l-.108.631c-.044.293-.078.584-.104.875l-.052.697c-.013.266-.018.531-.018.797s.004.532.018.797l.052.697c.027.291.062.584.104.875l.108.631c.058.299.12.596.194.893l.178.637c.082.275.166.553.264.826.087.244.185.484.282.725.096.23.188.461.295.689.136.289.284.57.437.852l.199.391.092.143c.176.301.37.588.566.875l.314.467c.189.258.396.504.603.752l.414.496c.18.197.373.385.563.574.19.191.377.387.575.564l.496.416c.248.205.494.412.752.602l.463.316c.288.197.577.391.875.566l.142.092.39.199c.28.152.562.303.851.438l.083.043.405.166c1.082.461 2.212.787 3.364 1.002l4.565-7.916c-.535.107-1.089.168-1.658.168z" fill="#888"/>
@@ -32,13 +38,13 @@
                                 <path class="AC-extend" d="M29.838 7.991c-.335-.58-.71-1.121-1.104-1.643l-.129-.16c-.354-.457-.729-.891-1.125-1.299l-.268-.275c-.435-.428-.886-.834-1.359-1.205l-.104-.088c-.527-.406-1.079-.771-1.648-1.105l-.114-.076-.433-.225c-.25-.135-.499-.27-.756-.391-.247-.115-.496-.217-.745-.32l-.627-.25c-.303-.109-.609-.201-.916-.295l-.51-.148c-.334-.086-.671-.154-1.007-.219l-.476-.094c-.328-.053-.656-.086-.985-.117l-.539-.055c-.282-.018-.563-.016-.847-.02l-.694-.004-.54.039c-1.194.08-2.388.293-3.561.648l-.243.066c-.297.096-.59.213-.884.328l-.564.223c-.242.103-.481.224-.721.343-.232.114-.465.229-.693.356l-.26.133-.384.248c-.227.14-.452.283-.673.435-.216.151-.429.305-.637.463l-.55.439c-.227.19-.448.383-.663.583l-.408.398c-.237.238-.469.48-.688.73l-.149.18 4.551 7.895c1.045-3.201 4.052-5.518 7.603-5.518h13.85z" fill="#888"/>$
                             </svg>
                         </a>
-                        <a class="AC-extend" href="#">
+                        <a class="AC-extend" href="https://addons.mozilla.org/fr/firefox/addon/accropolis-notification-live/">
                             <svg class="AC-extend" enable-background="new -0.002 -0.501 32 31" height="31" viewBox="-0.002 -0.501 32 31" width="32" xmlns="http://www.w3.org/2000/svg">
                                 <path class="AC-extend" d="M15.998-.501c8.284 0 15 6.715 15 15 0 8.283-6.716 15-15 15s-15-6.717-15-15c0-8.286 6.716-15 15-15z" fill="#666"/>
                                 <path class="AC-extend" d="M18.562 30.274c7.614-1.229 13.437-7.814 13.437-15.775l-.146.181c.228-1.513.188-2.861-.125-4.037-.112.871-.234 1.397-.357 1.608-.001-.068-.018-.976-.307-2.243-.137-.924-.338-1.793-.616-2.596.068.368.108.675.128.953-1.162-3.118-4.023-6.957-11.072-6.867 0 0 2.479.262 3.646 2.029 0 0-1.193-.285-2.095.152 1.099.438 2.052.896 2.863 1.374l.07.043c.208.125.385.253.573.38 1.498 1.052 2.888 2.553 2.782 4.489-.323-.51-.753-.844-1.304-1.012.679 2.66.746 4.857.197 6.59-.377-1.154-.724-1.846-1.033-2.09.431 3.566-.15 6.203-1.739 7.922.302-1.049.422-1.908.354-2.586-1.867 2.822-3.991 4.281-6.372 4.371-.941-.008-1.83-.15-2.666-.426-1.228-.415-2.339-1.124-3.328-2.13 1.544.129 2.954-.139 4.195-.772l2.033-1.332-.008-.006c.264-.1.512-.092.754.021.496-.068.67-.334.504-.783-.24-.334-.603-.637-1.069-.904-1.017-.531-2.079-.447-3.187.26-1.055.6-2.07.576-3.058-.062-.647-.447-1.272-1.049-1.876-1.801l-.24-.355c-.113.852.015 1.945.398 3.291l.008.018-.008-.016c-.384-1.346-.511-2.442-.398-3.293v-.008c.029-.744.337-1.154.924-1.246l-.249-.021.251.021c.663.061 1.424.213 2.282.463.144-.828-.045-1.695-.564-2.584v-.016c.806-.752 1.521-1.299 2.132-1.648.271-.145.429-.365.481-.662l.022-.016.008-.008.03-.029c.158-.236.105-.426-.165-.594-.565.031-1.131-.008-1.695-.121l-.008.023c-.233-.068-.527-.275-.889-.625l-.927-.912-.278-.219v.029h-.008l.008-.037-.053-.055.075-.053c.128-.691.339-1.285.64-1.795l.068-.061c.302-.502.881-1.041 1.732-1.617-1.582.197-3.013.91-4.285 2.143-1.055-.387-2.305-.305-3.744.25l-.173.132-.013.007.188-.138.008-.008c-.905-.416-1.515-1.611-1.809-3.564-1.152 1.141-1.71 3.178-1.673 6.119l-.33.499-.085.058-.017.016-.007.007-.016.033c-.175.274-.416.688-.72 1.244-.437.786-.584 1.446-.627 2.021l-.004.007.002.019-.014.151.025-.04c.003.133.006.267.04.387l.934-.768c-.339.859-.564 1.77-.678 2.736l-.027.442-.293-.335c0 3.428 1.088 6.597 2.924 9.201l.055.086.088.105c1.32 1.813 3.006 3.338 4.958 4.464 1.403.831 2.911 1.413 4.519 1.759l.331.074c.333.064.674.112 1.016.155.253.033.506.065.762.087l.34.039.483.003.525.026.418-.021.689-.034c.409-.028.812-.073 1.212-.131l.243-.036zm-9.409-16.75h0zm19.527-2.741l-.007.131.005-.132.002.001z" fill="#888"/>
                             </svg>
                         </a>
-                        <a class="AC-extend" href="#">
+                        <a class="AC-extend" href="https://auth.opera.com/account/login?return_url=https%3A//addons.opera.com/developer/extensions/details/accropolis-notification-live/0.0.1/">
                             <svg class="AC-extend" enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.0" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g>
                                 <path class="AC-extend" d="M26.737,4.375c2.842,2.917,4.264,6.75,4.264,11.5c0,4.334-1.422,8.104-4.264,11.312   C23.895,30.396,20.295,32,15.938,32c-4.316,0-7.885-1.604-10.706-4.812C2.41,23.979,1,20.209,1,15.875   c0-4.75,1.39-8.583,4.171-11.5C7.95,1.459,11.54,0,15.938,0C20.295,0,23.895,1.459,26.737,4.375z M21.291,11.062   c-0.124-1.291-0.373-2.552-0.747-3.781c-0.373-1.229-0.944-2.177-1.711-2.844c-0.769-0.666-1.733-1-2.895-1   c-1.162,0-2.116,0.323-2.863,0.969C12.328,5.053,11.778,6,11.426,7.25S10.835,9.75,10.71,11c-0.124,1.25-0.187,2.771-0.187,4.562   c0,1.125,0.021,2.073,0.062,2.844c0.04,0.771,0.113,1.709,0.218,2.812c0.103,1.104,0.27,2.031,0.498,2.781   c0.228,0.75,0.538,1.49,0.934,2.219c0.394,0.729,0.902,1.281,1.524,1.656s1.348,0.562,2.179,0.562c0.829,0,1.565-0.188,2.21-0.562   c0.643-0.375,1.161-0.927,1.556-1.656C20.098,25.49,20.42,24.75,20.669,24s0.436-1.677,0.56-2.781   c0.125-1.104,0.197-2.052,0.219-2.844c0.02-0.791,0.03-1.729,0.03-2.812C21.478,13.854,21.416,12.354,21.291,11.062z" fill="#888"/><g/>
                             </svg>
@@ -53,7 +59,6 @@
                                 <polygon  class="AC-extend" fill="#666" points="17,18 23.777,9.222 15,16  "/>
                                 <polygon  class="AC-extend" fill="#fff" points="15,16 8.221,24.777 17,18  "/></g><g/><g/><g/><g/><g/><g/></svg>
                             </a>
-
                     </p>
 
                 </div>
@@ -72,9 +77,9 @@
                 </article>
             </section>
         <section class="AC-main row">
-            <aside class="AC-left col-md-3">
+            <aside class="AC-left col-md-3 hidden-xs hidden-sm">
                 <div class="AC-discord-title col-md-12">
-                    <img class="col-md-2" src="./assets/img/DiscordLogo.svg" alt="Discord Logo">
+                    <img class="col-md-2" src="./assets/svg/DiscordLogo.svg" alt="Discord Logo">
                     <h3 class="col-md-10">Discord</h3>
                 </div>
                 <article class="col-md-12">
@@ -88,7 +93,7 @@
                 </article>
             </aside>
             <div class="AC-prognew col-md-9">
-                <article class="col-md-12 AC-bnews">
+                <article class="col-md-12 hidden-xs hidden-sm AC-bnews">
                         <h3 class="pull-left">Breaking News</h3>
                         <div class="AC-defileParent">
                                 <span class="AC-defile" data-text="- Penelope Gate : JFC annonce sa candidature - Lorem Ipsum - Ipsum Lorem - Ca win en Lan " >
@@ -96,23 +101,23 @@
                                 </span>
                         <p></p>
                 </article>
-                <article class="col-md-12 AC-prog">
+                <article class="col-md-12 col-xs-12 AC-prog">
                         <h3 class="pull-left">La Programmation</h3>
                 </article>
-                <article class="col-md-12 AC-prog-titre text-center">
-                    <figure class="col-md-2">
+                <article class="col-md-12 col-xs-12 AC-prog-titre text-center">
+                    <figure class="col-xs-offset-1 col-md-2 col-xs-2">
                         <p>Lundi</p>
                     </figure>
-                    <figure class="col-md-2">
+                    <figure class="col-md-2 col-xs-2">
                         <p>Mardi</p>
                     </figure>
-                    <figure class="col-md-2">
+                    <figure class="col-md-2 col-xs-2">
                         <p>Mercredi</p>
                     </figure>
-                    <figure class="col-md-2">
+                    <figure class="col-md-2 col-xs-2">
                         <p>Jeudi</p>
                     </figure>
-                    <figure class="col-md-2">
+                    <figure class="col-md-2 col-xs-2">
                         <p>Vendredi</p>
                     </figure>
                 </article>
@@ -123,7 +128,7 @@
                     <h2 class="pull-left">Les derniers Replay</h2>
                     <article class="AC-vidreplay col-md-12">
                     <?php for ($i=0;$i<9;$i++) : ?>
-                        <div class="col-md-4">
+                        <div class="col-md-4 col-xs-6">
                             <a href="#">
                                 <img width="80%" src="./assets/img/placeholderstream.jpg" alt="">
                                 <p class="AC-titre-replay">QAG Commentées !</p>
@@ -154,9 +159,10 @@
             </article>
         </section>
     </main>
-    <aside class="AC-Chat col-md-3">
+    <aside class="AC-Chat hidden-xs hidden-sm col-md-3">
         Chat twitch
     </aside>
+    <?php include('template/footer.html') ?>
 </body>
 
 </html>
